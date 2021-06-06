2a3f1726537a53c1d5cd871f801e3f5164d355f4
MichelleChiasson <mitch.inbox@gmail.com>
2021-06-05 21:35:54 -0400

    Added form elements & worked on event listener function. Retrieved user data!

--- script.js
+++ script.js
@@ -5,22 +5,18 @@
 // create a function that will loop through the selected day of the week(based on the user's value)
 // call the randomizer function, to obtain the word and return it.
 
-// fetch(`https://wordsapiv1.p.rapidapi.com/words/tenacity`, {
-//   'method': 'GET',
-//   'headers': {
-//     'x-rapidapi-key': 'fc2f5f842fmsh9040c3bedc1e59ep161878jsn8475b76c4099',
-//     'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com'
-//   }
-// })
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(result => {
-//     console.log(result);
-//   })
-
 const wordApp = {};
 wordApp.url = `https://wordsapiv1.p.rapidapi.com/words/lollygag`;
+
+wordApp.usersChoice = () => {
+  wordApp.form = document.querySelector('form');
+
+  wordApp.form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const day = document.querySelector('input[type=radio]:checked').value;
+    console.log(day);
+  })
+}
 
 
 wordApp.getData = () => {
@@ -42,6 +38,7 @@ wordApp.getData = () => {
 
 
 wordApp.init = () => {
+  wordApp.usersChoice();
   wordApp.getData();
 }
 
